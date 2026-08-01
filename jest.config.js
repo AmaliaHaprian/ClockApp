daa0acc1c609e3ffd96cf8c0c3d83909c3a11c70
migration runner <runner@migration.local>
2026-08-01 21:17:52 +0000

    agent (agent)

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
-module.exports = {
+/** @type {import('jest').Config} */
+const config = {
   testEnvironment: "node",
   testPathIgnorePatterns: ["/node_modules/", "/dist/"],
 };
+
+module.exports = config;
