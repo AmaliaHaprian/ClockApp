15158a4753d75eefabc8d5e788124bfe3e28ce83
migration runner <runner@migration.local>
2026-08-04 14:58:14 +0000

    agent (fixer)

--- src/index.test.js
+++ src/index.test.js
@@ -1,17 +1,20 @@
+import React from "react";
+
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
 jest.mock("react-dom/client", () => ({
   createRoot: jest.fn(),
 }));
 
 jest.mock("./App", () => {
-  const React = require("react");
-
-  function App() {
+  function MockApp() {
     return React.createElement("div", { className: "app" }, "App");
   }
 
   return {
     __esModule: true,
-    default: App,
+    default: MockApp,
   };
 });
 
@@ -19,23 +22,24 @@ describe("src/index", () => {
   beforeEach(() => {
     jest.resetModules();
     document.body.innerHTML = '<div id="root"></div>';
+    createRoot.mockReset();
   });
 
   test("creates one React 18 root for #root and renders App into it", () => {
     const container = document.getElementById("root");
     const render = jest.fn();
-    const { createRoot } = require("react-dom/client");
 
     createRoot.mockReturnValue({ render });
 
-    require("./index");
+    jest.isolateModules(() => {
+      require("./index");
+    });
 
     expect(createRoot).toHaveBeenCalledTimes(1);
     expect(createRoot).toHaveBeenCalledWith(container);
     expect(render).toHaveBeenCalledTimes(1);
 
     const renderedElement = render.mock.calls[0][0];
-    const App = require("./App").default;
 
     expect(renderedElement.type).toBe(App);
     expect(renderedElement.props).toEqual({});
