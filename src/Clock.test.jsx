8d17b4b52c87acaa7a4933b23f5e545c49b224bd
migration runner <runner@migration.local>
2026-08-04 13:28:36 +0000

    agent (test_agent)

--- src/Clock.test.jsx
+++ src/Clock.test.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import renderer, { act } from "react-test-renderer";
 
 import Clock from "./Clock";
 import { subscribe } from "./clock-source";
 
 jest.mock("./clock-source");
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
 
 test("subscribes on mount and renders a clock div", () => {
   subscribe.mockImplementation(() => jest.fn());
@@ -17,7 +21,9 @@ test("subscribes on mount and renders a clock div", () => {
   expect(subscribe).toHaveBeenCalledTimes(1);
   expect(root.toJSON().props.className).toBe("clock");
 
-  root.unmount();
+  act(() => {
+    root.unmount();
+  });
 });
 
 test("unsubscribes on unmount", () => {
@@ -54,4 +60,44 @@ test("re-renders with the time passed to the subscribed callback", () => {
   expect(root.toJSON().children[0]).toBe(
     new Date("2026-08-01T09:30:00").toLocaleTimeString(),
   );
+
+  act(() => {
+    root.unmount();
+  });
+});
+
+test("cleans up subscriptions correctly under StrictMode remounting", () => {
+  const unsubscribeOne = jest.fn();
+  const unsubscribeTwo = jest.fn();
+  subscribe
+    .mockImplementationOnce(() => unsubscribeOne)
+    .mockImplementationOnce(() => unsubscribeTwo);
+
+  let root;
+  act(() => {
+    root = renderer.create(
+      <StrictMode>
+        <Clock />
+      </StrictMode>,
+    );
+  });
+
+  expect(subscribe).toHaveBeenCalledTimes(1);
+
+  act(() => {
+    root.update(
+      <StrictMode>
+        <Clock />
+      </StrictMode>,
+    );
+  });
+
+  expect(subscribe).toHaveBeenCalledTimes(1);
+
+  act(() => {
+    root.unmount();
+  });
+
+  expect(unsubscribeOne).toHaveBeenCalledTimes(1);
+  expect(unsubscribeTwo).toHaveBeenCalledTimes(0);
 });
